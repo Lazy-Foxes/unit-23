5f9af3457128cfc54a7f8e514fb08ff708e987bf
RedBurningPhoenix <Warete4@gmail.com>
2026-05-07 20:43:16 +0500

    Centcomm

--- Content.Server/Shuttles/Components/StationCentcommComponent.cs
+++ Content.Server/Shuttles/Components/StationCentcommComponent.cs
@@ -23,7 +23,7 @@ public sealed partial class StationCentcommComponent : Component
     public float ShuttleIndex;
 
     [DataField]
-    public ResPath Map = new("/Maps/centcomm.yml");
+    public ResPath Map = new("/Maps/_Maid/CentcommMaid.yml"); // Maid - re-add classic centcomm
 
     /// <summary>
     /// Centcomm entity that was loaded.
